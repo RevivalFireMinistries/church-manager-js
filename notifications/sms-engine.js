e8aa40aafd5c0cdf5f1cc249b97a5a0029d95102
Russel Mupfumira <russel@resafrica.com>
2016-10-27 12:51:33 +0200

    SMS unit tests and integration with WINSMS API

--- notifications/sms-engine.js
+++ notifications/sms-engine.js
@@ -1,7 +1,39 @@
-
+var http = require('http');
+var fs    = require('fs');
+var util  = require('util');
 
 module.exports = {
-    sendSMS: function (message,mobileNumber) {
+    sendSMS: function (message,mobileNumber,callbackFunction) {
+        var options = {
+            host: 'http://www.winsms.co.za',
+            path: '/api/batchmessage.asp?User=luke@rfm.org.za&Password=password85&message='+
+            encodeURIComponent(message)+'&Numbers='+mobileNumber
+        };
+        callback = function(response) {
+            var str = '';
 
+            //another chunk of data has been recieved, so append it to `str`
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+
+            //the whole response has been recieved, so we just print it out here
+            response.on('end', function () {
+                console.log(str);
+                callbackFunction(str);
+            });
+        }
+
+        http.request(options, callback).end();
+    },
+
+    sendMessage: function (message,mobileNumber) {
+        var url = 'http://www.winsms.co.za/api/batchmessage.asp?User=luke@rfm.org.za&Password=password85&message='+
+            encodeURIComponent(message)+'&Numbers='+mobileNumber;
+        fs.open(url, 'w', function(err, fd) {
+            for (var i = 0; i < 100; i++)
+                fs.write(fd, util.format('line %d\n', i));
+            fs.close(fd);
+        });
     }
 }
--- test/utils-test.js
+++ test/utils-test.js
@@ -1,10 +1,12 @@
 var util = require('./../utils/Utils');
+var sms = require('./../notifications/sms-engine');
+var config = require('./../config/config');
 
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 
 
-describe('Date format', function() {
+describe('Tests : ', function() {
     it('display a human friendly date', function() {
         console.log(util.getFriendlyDate(new Date()));
         expect(0).to.equal(0);
@@ -12,6 +14,15 @@ describe('Date format', function() {
 
     it('display money', function() {
         console.log(util.formatMoney(1200.78787));
+        expect(0).to.equal(0);
+    });
+
+
+    it('send sms', function() {
+        sms.sendSMS("test sms",27722621278,function(response){
+            console.log(response);
+        });
+        sms.sendMessage("test sms the second one",27722621278);
         expect(0).to.equal(0);
     });
 });
