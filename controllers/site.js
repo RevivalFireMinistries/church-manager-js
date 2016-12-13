6b1c7c67885c4d4923166b80e67c039d7f127f89
Russel Mupfumira <russel@resafrica.com>
2016-12-13 19:02:37 +0200

    enable sms testing

--- controllers/site.js
+++ controllers/site.js
@@ -12,9 +12,9 @@ exports.index = function(req, res){
         comment    :"Wonderful service we had with a very long comment to see if tghis will actually break the styling of the email report because you get people who type a lot!\nThe word was awesome\nwe need a bigger venue"
     }
    email.sendEventEmail(event);
-    /*sms.sendSMS("test sms the second one",27722621278,function(resp){
+    sms.sendSMS("test sms the second one",27722621278,function(resp){
         console.log(resp);
-    });*/
+    });
     res.send("Hello!");
 };
 
