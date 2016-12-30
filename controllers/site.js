069f22dfdb756a0711b180d5c43321b8693840d0
Russel Mupfumira <russel@resafrica.com>
2016-12-30 15:09:58 +0200

    commented out sms sending

--- controllers/site.js
+++ controllers/site.js
@@ -3,15 +3,15 @@
 
 exports.index = function(req, res){
     event = {
-        assembly   : 1,
+        assembly   : 6,
         attendance : 134,
         eventDate  : new Date(),
-        eventType  : "Midweek Service",
+        eventType  : "TEST REPORT ",
         offerings  :779.89,
         tithes     :60000.00,
-        comment    :"Wonderful service we had with a very long comment to see if tghis will actually break the styling of the email report because you get people who type a lot!\nThe word was awesome\nwe need a bigger venue"
+        comment    :"THIS IS A TEST REPORT!"
     }
-   email.sendEventEmail(event);
+   //email.sendEventEmail(event);
     sms.sendSMS("test sms the second one",27722621278,function(resp){
         console.log(resp);
     });
